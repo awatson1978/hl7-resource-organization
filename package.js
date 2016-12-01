e006e2118c912e40e8aed1c2bf3c6326b835a2bc
Abigail Watson <awatson1978@gmail.com>
2016-12-01 12:39:01 -0600

    bump deps

--- package.js
+++ package.js
@@ -17,7 +17,7 @@ Package.onUse(function (api) {
   api.use('prime8consulting:meteor-oauth2-server@0.0.2');
 
   api.use('clinical:base-model@1.3.5');
-  api.use('clinical:hl7-resource-datatypes@0.60');
+  api.use('clinical:hl7-resource-datatypes@0.6.0');
 
   api.addFiles('lib/hl7-resource-organization.js', ['client', 'server']);
   api.addFiles('server/rest.js', 'server');
