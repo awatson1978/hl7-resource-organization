07373fee98ae8d1ea9e96bf16170024aa4a55289
Abigail Watson <awatson1978@gmail.com>
2017-03-07 15:50:56 -0600

    extracted pub/sub functions to clinical:autopublish

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-organization',
-  version: '1.1.1',
+  version: '1.2.0',
   summary: 'HL7 FHIR Resource - Organization',
   git: 'https://github.com/clinical-meteor/hl7-resource-organization',
   documentation: 'README.md'
