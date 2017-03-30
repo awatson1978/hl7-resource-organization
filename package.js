f0972121dbc40146ad736b866f76b0e48fb2fec5
Abigail Watson <awatson1978@gmail.com>
2017-03-30 13:42:02 -0500

    Organizations.prepForFhirTransfer()

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-organization',
-  version: '1.3.2',
+  version: '1.3.3',
   summary: 'HL7 FHIR Resource - Organization',
   git: 'https://github.com/clinical-meteor/hl7-resource-organization',
   documentation: 'README.md'
