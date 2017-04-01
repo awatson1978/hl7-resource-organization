e4ce808af84723baee3d07d9bf86010a29cf76aa
Abigail Watson <awatson1978@gmail.com>
2017-03-31 19:24:51 -0500

    not sure whats wrong here

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-organization',
-  version: '1.3.8',
+  version: '1.3.9',
   summary: 'HL7 FHIR Resource - Organization',
   git: 'https://github.com/clinical-meteor/hl7-resource-organization',
   documentation: 'README.md'
