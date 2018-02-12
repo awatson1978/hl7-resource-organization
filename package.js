034c1e54961dc3de481fa34a13fc8016d75985aa
Abigail Watson <awatson1978@gmail.com>
2018-02-12 17:23:36 -0600

    workflow/react refactoring

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-organization',
-  version: '1.4.1',
+  version: '1.5.0',
   summary: 'HL7 FHIR Resource - Organization',
   git: 'https://github.com/clinical-meteor/hl7-resource-organization',
   documentation: 'README.md'
@@ -14,17 +14,28 @@ Package.onUse(function (api) {
   api.use('aldeed:simple-schema@1.3.3');
   api.use('aldeed:collection2@2.5.0');
   api.use('simple:json-routes@2.1.0');
-  api.use('clinical:fhir-vault-server@0.0.3', ['client', 'server'], {weak: true});
+  api.use('momentjs:moment@2.17.1');
+  api.use('ecmascript@0.9.0');
+  api.use('session');
+  api.use('http');
+  api.use('react-meteor-data@0.2.15');
 
+  api.use('clinical:glass-ui@2.1.4');
+  api.use('clinical:extended-api@2.2.2');
   api.use('clinical:base-model@1.3.5');
-  api.use('clinical:hl7-resource-datatypes@3.0.0');
+  api.use('clinical:user-model@1.5.0');
+  api.use('clinical:hl7-resource-datatypes@3.0.1');
   api.use('clinical:hl7-resource-bundle@1.3.10');
+  api.use('matb33:collection-hooks@0.7.15');
 
-  api.addFiles('lib/hl7-resource-organization.js', ['client', 'server']);
+  api.addFiles('lib/Organizations.js', ['client', 'server']);
   api.addFiles('server/rest.js', 'server');
   api.addFiles('server/initialize.js', 'server');
+  api.addFiles('server/methods.js', 'server');
 
   api.export('Organization');
   api.export('Organizations');
   api.export('OrganizationSchema');
+
+  api.mainModule('index.jsx', 'client');
 });
