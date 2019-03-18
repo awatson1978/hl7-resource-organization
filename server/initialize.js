fc5806d31b31c56ff247f9124788d108737133d0
Abigail Watson <awatson1978@gmail.com>
2019-03-18 03:19:01 -0500

    bump glass-ui

--- server/initialize.js
+++ server/initialize.js
@@ -1,5 +1,10 @@
+import { grocers } from './grocers.json'
+
 // if the database is empty on server start, create some sample data.
 Meteor.startup(function () {
+
+  console.log('grocers', grocers)
+
   if (process.env.INITIALIZE) {
     console.log('INITIALZING');
     if (Organizations.find().count() === 0) {
@@ -10,3 +15,5 @@ Meteor.startup(function () {
     }
   }
 });
+
+
