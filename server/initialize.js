17d405ac2b967b43a8147fb7a2a5325644b4b2bd
Abigail Watson <awatson1978@gmail.com>
2019-03-18 10:36:02 -0500

    grocers.json

--- server/initialize.js
+++ server/initialize.js
@@ -1,9 +1,9 @@
-import { grocers } from './grocers.json'
+// import { grocers } from './grocers.json'
 
 // if the database is empty on server start, create some sample data.
 Meteor.startup(function () {
 
-  console.log('grocers', grocers)
+  // console.log('grocers', grocers)
 
   if (process.env.INITIALIZE) {
     console.log('INITIALZING');
